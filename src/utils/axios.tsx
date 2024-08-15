90abbbe1b5093323d32d700206a4a861bb9a0eaf
HectorVu <hung.vm2574@gmail.com>
2024-08-15 20:38:39 +0800

    export BASE_URL

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,7 +2,7 @@ import { getStoreUuid } from "../helper/getStoreUuid";
 import { getToken } from "../helper/getToken";
 import axios from "axios";
 
-const BASE_URL = "https://big-umbrella-c5c3450b8837.herokuapp.com/";
+export const BASE_URL = "https://big-umbrella-c5c3450b8837.herokuapp.com/";
 // export const BASE_URL = "http://localhost:8080/";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
