5cea6ebcc74be6d7e7b9851af030d7cb4fa21427
HectorVu <hung.vm2574@gmail.com>
2024-08-16 21:04:02 +0800

    add Notification on mobile menu bar

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -46,7 +46,7 @@ const MenubarDemo = () => {
   const [selectedStore, setSelectedStore] = useState<string | undefined>(
     localStorageStoreUuid || undefined
   );
-  
+
   const dispatch = useDispatch();
 
   const fetchAllStore = useCallback(async () => {
@@ -102,9 +102,8 @@ const MenubarDemo = () => {
     <div className="mb-[100px] relative z-10">
       <div className="flex justify-between items-center font-bold text-lg">
         <div
-          className={`absolute top-[60px] left-0 right-0 bg-white shadow-md rounded-md md:hidden ${
-            isOpen ? "block" : "hidden"
-          }`}
+          className={`absolute top-[60px] left-0 right-0 bg-white shadow-md rounded-md md:hidden ${isOpen ? "block" : "hidden"
+            }`}
         >
           <SelectStore
             handleStoreChange={handleStoreChange}
@@ -123,17 +122,17 @@ const MenubarDemo = () => {
                   }
                   setIsOpen(false);
                 }}
-                className={`cursor-pointer py-2 mb-4 px-3 outline-none select-none font-bold leading-none rounded text-slate-900 text-[15px] lg:text-base flex items-center justify-between gap-[4px] hover:underline ${
-                  currentPath === menuItem.path &&
+                className={`cursor-pointer py-2 mb-4 px-3 outline-none select-none font-bold leading-none rounded text-slate-900 text-[15px] lg:text-base flex items-center justify-between gap-[4px] hover:underline ${currentPath === menuItem.path &&
                   "underline underline-offset-4"
-                }`}
+                  }`}
               >
                 {menuItem.label}
               </div>
             </div>
           ))}
         </div>
-        <div className="absolute right-0 top-1 flex md:hidden bg-white p-[3px] mt-5 w-[10%] mx-4 justify-center rounded-md border-2">
+        <div className="absolute right-0 top-1 flex justify-evenly md:hidden bg-white p-[3px] mt-5 w-[20%] mx-4 rounded-md border-2">
+          <NotificationIcon />
           <div className={`cursor-pointer`} onClick={toggleMenu}>
             <svg
               className="w-6 h-6"
@@ -179,9 +178,8 @@ const MenubarDemo = () => {
                   navigate(menuItem.path);
                 }
               }}
-              className={` cursor-pointer py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] hover:underline hover:underline-offset-4 ${
-                currentPath === menuItem.path && "underline underline-offset-4"
-              }`}
+              className={` cursor-pointer py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] hover:underline hover:underline-offset-4 ${currentPath === menuItem.path && "underline underline-offset-4"
+                }`}
             >
               {menuItem.label}
             </div>
