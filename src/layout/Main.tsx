29ac96ae7f4b74f4f9108dc26f56aa53699ece54
HectorVu <hung.vm2574@gmail.com>
2024-08-31 05:48:01 +0800

    Fix issue with displaying session expired at root url

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -4,7 +4,7 @@ import useAuthHomePageRedirect from "../hooks/useAuthHomePageRedirect";
 
 const Main = () => {
   const location = useLocation();
-  const hideMenubarPaths = ["/login", "/session-expired", "/404"];
+  const hideMenubarPaths = ["/login", "/session-expired", "/404", "/"];
 
   useAuthHomePageRedirect();
   return (
