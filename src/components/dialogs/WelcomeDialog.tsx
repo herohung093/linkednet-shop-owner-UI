13b23a50c6c1dbb880a608e4a59ead7beeae0bb0
HectorVu <hung.vm2574@gmail.com>
2024-10-12 21:34:15 +0800

    add WelcomeDialog to popup when user first login

---
+++ src/components/dialogs/WelcomeDialog.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+interface WelcomeDialogProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const WelcomeDialog: React.FC<WelcomeDialogProps> = ({ open, onClose }) => {
+  const navigate = useNavigate();
+
+  const handleCreateStore = () => {
+    onClose();
+    navigate("/create-store");
+  };
+
+  const handleClose = (event: object, reason: string) => {
+    if (reason !== "backdropClick") {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="welcome-dialog-title"
+      aria-describedby="welcome-dialog-description"
+      disableEscapeKeyDown
+    >
+      <DialogTitle id="welcome-dialog-title">Welcome!</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="welcome-dialog-description">
+          You have just created an account. There is no store associated with
+          this account yet. Please create a store to get started.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCreateStore} color="primary" variant="contained">
+          Create Store
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default WelcomeDialog;
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,63 +1,100 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Grid, Paper, Typography, Box } from "@mui/material";
 import BookingChart from "../components/BookingCharts";
 import StaffChart from "../components/StaffChart";
 import RevenueChart from "../components/RevenueChart";
 import UpComingBooking from "../components/UpComingBooking";
 import withAuth from "../components/HOC/withAuth";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux toolkit/store";
+import WelcomeDialog from "../components/dialogs/WelcomeDialog";
 
 const Dashboard: React.FC = () => {
+  const [openWelcomeDialog, setOpenWelcomeDialog] = useState<boolean>(false);
+
+  const storeConfigRedux = useSelector(
+    (state: RootState) => state.storesList.storesList
+  );
+  const storeConfig = useMemo(() => {
+    if (storeConfigRedux) {
+      return storeConfigRedux.slice().sort((a, b) => a.id - b.id);
+    }
+    return [];
+  }, [storeConfigRedux]);
+
+  const handleCloseWelcomeDialog = () => {
+    setOpenWelcomeDialog(false);
+  };
+
+  useEffect(() => {
+    if (storeConfig.length === 0) {
+      setOpenWelcomeDialog(true);
+    }
+  }, [storeConfig]);
+
   return (
-    <Box sx={{ padding: "24px" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={8}>
-          <BookingChart />
-        </Grid>
+    <div>
+      <Box sx={{ padding: "24px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={8}>
+            <BookingChart />
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <Paper elevation={3} sx={{ padding: "16px", height: "100%" }}>
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: "bold",
-                color: "primary.main",
-                marginTop: "5px",
-                textTransform: "uppercase",
-                borderBottom: "2px solid",
-                borderColor: "primary.main",
-              }}
-            >
-              Up Coming Bookings
-            </Typography>
-            <Box sx={{ marginTop: "1rem" }}>
-              <UpComingBooking />
-            </Box>
-          </Paper>
-        </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper elevation={3} sx={{ padding: "16px", height: "100%" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontWeight: "bold",
+                  color: "primary.main",
+                  marginTop: "5px",
+                  textTransform: "uppercase",
+                  borderBottom: "2px solid",
+                  borderColor: "primary.main",
+                }}
+              >
+                Up Coming Bookings
+              </Typography>
+              <Box sx={{ marginTop: "1rem" }}>
+                <UpComingBooking />
+              </Box>
+            </Paper>
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <Paper elevation={3} sx={{ padding: "16px", height: "100%" }}>
-            <Typography variant="h6" sx={{
-                fontWeight: "bold",
-                color: "primary.main",
-                marginTop: "5px",
-                textTransform: "uppercase",
-                borderBottom: "2px solid",
-                borderColor: "primary.main",
-              }}>Most Value Customers</Typography>
-            {/* Add analytics content here */}
-          </Paper>
-        </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper elevation={3} sx={{ padding: "16px", height: "100%" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontWeight: "bold",
+                  color: "primary.main",
+                  marginTop: "5px",
+                  textTransform: "uppercase",
+                  borderBottom: "2px solid",
+                  borderColor: "primary.main",
+                }}
+              >
+                Most Value Customers
+              </Typography>
+              {/* Add analytics content here */}
+            </Paper>
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <RevenueChart />
-        </Grid>
+          <Grid item xs={12} md={4}>
+            <RevenueChart />
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <StaffChart />
+          <Grid item xs={12} md={4}>
+            <StaffChart />
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+
+      <WelcomeDialog
+        open={openWelcomeDialog}
+        onClose={handleCloseWelcomeDialog}
+      />
+    </div>
   );
 };
 
