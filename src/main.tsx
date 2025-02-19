e70b9bd95e7f994ad0cb2f602faeec82827e1b09
HectorVu <hung.vm2574@gmail.com>
2025-02-19 21:15:35 +0800

    feat: integrate Google Analytics for tracking user interactions

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { Theme } from "@radix-ui/themes";
 import { Provider } from "react-redux";
 import store from "./redux toolkit/store";
 import './i18n';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-58XC1WLCYW");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
